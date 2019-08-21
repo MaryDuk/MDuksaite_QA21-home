2c0f51f89fb8affa478666814b0c574737d4d911
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-08-21 21:05:56 +0300

    Added Module SearchWikiTest

---
+++ wikipedia-web-tests/src/test/java/com/telran/selenium/SearchWikiTest.java
@@ -0,0 +1,23 @@
+package com.telran.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SearchWikiTest {
+
+    WebDriver driver;
+    @Test
+public void setUp () throws InterruptedException {
+
+    driver = new ChromeDriver();
+    driver.get("https://en.wikipedia.org");
+    driver.findElement(By.id("searchInput")).sendKeys("TRAFFORD PARK");
+    driver.findElement(By.id("searchButton")).click();
+    Thread.sleep(7000);
+    driver.quit();
+
+}
+
+}
