5e1d54b9fc088a7ece1f21fe0accc59a2d1994cb
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-14 20:36:07 +0300

    @Before Class and Method added, Assert added

--- wikipedia-web-tests/src/test/java/com/telran/selenium/LoginToWiki.java
+++ wikipedia-web-tests/src/test/java/com/telran/selenium/LoginToWiki.java
@@ -1,29 +1,30 @@
 package com.telran.selenium;
 
-import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginToWiki extends TestBase{
+
+   @BeforeClass
+   public void isOnHomePage () {
+       if (!app.isWikiHomePageOpened()){
+           app.openSite("https://en.wikipedia.org");
+       }
+   }
+
+   @BeforeMethod
+   public void isUserLoggedOff() {
+       if (!app.isLoginPresent()){
+           app.logOUt();
+       }
+   }
+
     @Test
-    public void logInWiki () throws InterruptedException {
-        driver.get("https://en.wikipedia.org");
-
-        click(By.id("pt-login"));
-
-        click(By.id("wpName1"));
-        clear(By.id("wpName1"));
-        type(By.id("wpName1"), "ASDFG");
-
-        click(By.id("wpPassword1"));
-        clear(By.id("wpPassword1"));
-        type(By.id("wpPassword1"), "123456");
-
-        click(By.id("wpRemember"));
-
-        click(By.id("wpLoginAttempt"));
-
-        Thread.sleep(7000);
-
+    public void logInWiki () {
+        app.login("MaryDuk", "trusty07");
+        boolean isPresent = app.isLogoutPresent();
+        Assert.assertEquals(isPresent, true);
     }
-
 }
