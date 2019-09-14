a0e394f4edd833185e817ccc806b46e3904feccf
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-14 20:36:31 +0300

    @Before Class added, Assert added

--- wikipedia-web-tests/src/test/java/com/telran/selenium/SearchWikiTest.java
+++ wikipedia-web-tests/src/test/java/com/telran/selenium/SearchWikiTest.java
@@ -1,25 +1,22 @@
 package com.telran.selenium;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class SearchWikiTest extends TestBase{
 
+    @BeforeClass
+    public void isOnHomePage () {
+        if (!app.isWikiHomePageOpened()){
+            app.openSite("https://en.wikipedia.org");
+        }
+    }
     @Test
-    public void searchTrafforParkTest () throws InterruptedException {
-        driver.get("https://en.wikipedia.org");
-        click(By.id("searchInput"));
-        clear(By.id("searchInput"));
-        type(By.id("searchInput"), "TRAFFORD PARK"+ Keys.ENTER);
-        //driver.findElement(By.id("searchButton")).click();
-        Thread.sleep(7000);
+    public void searchkTest ()  {
+        app.search("TRAFFORD PARK");
+        boolean isPresent = app.isSearchedElementPresent();
+        Assert.assertEquals(isPresent, true);
     }
 
 }
