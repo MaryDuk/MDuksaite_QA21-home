9c514b2ba4f0594c90c83be989c1faa0e57c2c56
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-21 20:06:23 +0300

    moved to package manager, browser constructor added, driver choice Chrome, Firefox added

--- wikipedia-web-tests/src/test/java/com/telran/selenium/manager/ApplicationManager.java
+++ wikipedia-web-tests/src/test/java/com/telran/selenium/manager/ApplicationManager.java
@@ -1,11 +1,9 @@
-package com.telran.selenium;
+package com.telran.selenium.manager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,10 +12,21 @@ public class ApplicationManager {
     WebDriver driver;
     SessionHelper sessionHelper;
     SearchHelper searchHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
 
     public void init() {
-        driver = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)){
+            driver = new ChromeDriver();
+        }if (browser.equals(BrowserType.FIREFOX)){
+            driver = new FirefoxDriver();
+        }
+        //driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
