bca35b48e2a089608a87078ecd413bc85fd4aba0
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-21 20:10:16 +0300

    moved to package tests, Property("browser", BrowserType.CHROME) added, if property was not defined, Chrome by default

---
+++ wikipedia-web-tests/src/test/java/com/telran/selenium/tests/TestBase.java
@@ -0,0 +1,23 @@
+package com.telran.selenium.tests;
+
+import com.telran.selenium.manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() {
+        app.init();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        app.stop();
+    }
+
+
+}
