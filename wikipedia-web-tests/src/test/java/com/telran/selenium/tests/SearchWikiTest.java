c4b23c46b3ac4d308a60dccc62e652cfca5f1f97
MaryDuk <54117390+MaryDuk@users.noreply.github.com>
2019-09-21 20:07:47 +0300

    moved to package tests

--- wikipedia-web-tests/src/test/java/com/telran/selenium/tests/SearchWikiTest.java
+++ wikipedia-web-tests/src/test/java/com/telran/selenium/tests/SearchWikiTest.java
@@ -1,9 +1,5 @@
-package com.telran.selenium;
+package com.telran.selenium.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
